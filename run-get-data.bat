d869203bd869e767e5b68a8fc6717f97fff18108
Jiri Stepan <jiri.stepan@etnetera.cz>
2015-09-23 20:13:08 +0200

    pridany komenty

--- run-get-data.bat
+++ run-get-data.bat
@@ -1,1 +1,6 @@
+# Ukazka toho, ze Rkovy skript jde spustit i z command line
+# Vystupem po spusteni jsou dva soubory v adresari 00-fitbit-demo
+# Jeden je ulozeny .rda jako vystup skriptu a druhy je .rout s logem provedeni skriptu
+# Vrimnete s na konci ze to i zmeri cas
+
 "c:\Program Files\R\R-3.2.1\bin\R.exe" CMD BATCH ".\00-fitbit-demo\0-getdata.R"
