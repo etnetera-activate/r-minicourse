18b272b5b531a23a0f9a70e23ad586879a7bd99c
Jiri Stepan <jiri.stepan@etnetera.cz>
2020-04-07 22:33:15 +0200

    korona

---
+++ 24-coronavirus/simple-vizualization.R
@@ -0,0 +1,66 @@
+library(dplyr)
+library(tidyr)
+library(ggplot2)
+library(scales)
+
+rm(list=ls())
+
+casesUrl <- "https://data.humdata.org/hxlproxy/data/download/time_series_covid19_confirmed_global_narrow.csv?dest=data_edit&filter01=merge&merge-url01=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vTglKQRXpkKSErDiWG6ycqEth32MY0reMuVGhaslImLjfuLU0EUgyyu2e-3vKDArjqGX7dXEBV8FJ4f%2Fpub%3Fgid%3D1326629740%26single%3Dtrue%26output%3Dcsv&merge-keys01=%23country%2Bname&merge-tags01=%23country%2Bcode%2C%23region%2Bmain%2Bcode%2C%23region%2Bsub%2Bcode%2C%23region%2Bintermediate%2Bcode&filter02=merge&merge-url02=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vTglKQRXpkKSErDiWG6ycqEth32MY0reMuVGhaslImLjfuLU0EUgyyu2e-3vKDArjqGX7dXEBV8FJ4f%2Fpub%3Fgid%3D398158223%26single%3Dtrue%26output%3Dcsv&merge-keys02=%23adm1%2Bname&merge-tags02=%23country%2Bcode%2C%23region%2Bmain%2Bcode%2C%23region%2Bsub%2Bcode%2C%23region%2Bintermediate%2Bcode&merge-replace02=on&merge-overwrite02=on&filter03=explode&explode-header-att03=date&explode-value-att03=value&filter04=rename&rename-oldtag04=%23affected%2Bdate&rename-newtag04=%23date&rename-header04=Date&filter05=rename&rename-oldtag05=%23affected%2Bvalue&rename-newtag05=%23affected%2Binfected%2Bvalue%2Bnum&rename-header05=value&filter06=clean&clean-date-tags06=%23date&filter07=sort&sort-tags07=%23date&sort-reverse07=on&filter08=sort&sort-tags08=%23country%2Bname%2C%23adm1%2Bname&tagger-match-all=on&tagger-default-tag=%23affected%2Blabel&tagger-01-header=province%2Fstate&tagger-01-tag=%23adm1%2Bname&tagger-02-header=country%2Fregion&tagger-02-tag=%23country%2Bname&tagger-03-header=lat&tagger-03-tag=%23geo%2Blat&tagger-04-header=long&tagger-04-tag=%23geo%2Blon&header-row=1&url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_confirmed_global.csv"
+deathsUrl <- "https://data.humdata.org/hxlproxy/data/download/time_series_covid19_deaths_global_narrow.csv?dest=data_edit&filter01=merge&merge-url01=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vTglKQRXpkKSErDiWG6ycqEth32MY0reMuVGhaslImLjfuLU0EUgyyu2e-3vKDArjqGX7dXEBV8FJ4f%2Fpub%3Fgid%3D1326629740%26single%3Dtrue%26output%3Dcsv&merge-keys01=%23country%2Bname&merge-tags01=%23country%2Bcode%2C%23region%2Bmain%2Bcode%2C%23region%2Bsub%2Bcode%2C%23region%2Bintermediate%2Bcode&filter02=merge&merge-url02=https%3A%2F%2Fdocs.google.com%2Fspreadsheets%2Fd%2Fe%2F2PACX-1vTglKQRXpkKSErDiWG6ycqEth32MY0reMuVGhaslImLjfuLU0EUgyyu2e-3vKDArjqGX7dXEBV8FJ4f%2Fpub%3Fgid%3D398158223%26single%3Dtrue%26output%3Dcsv&merge-keys02=%23adm1%2Bname&merge-tags02=%23country%2Bcode%2C%23region%2Bmain%2Bcode%2C%23region%2Bsub%2Bcode%2C%23region%2Bintermediate%2Bcode&merge-replace02=on&merge-overwrite02=on&filter03=explode&explode-header-att03=date&explode-value-att03=value&filter04=rename&rename-oldtag04=%23affected%2Bdate&rename-newtag04=%23date&rename-header04=Date&filter05=rename&rename-oldtag05=%23affected%2Bvalue&rename-newtag05=%23affected%2Binfected%2Bvalue%2Bnum&rename-header05=Value&filter06=clean&clean-date-tags06=%23date&filter07=sort&sort-tags07=%23date&sort-reverse07=on&filter08=sort&sort-tags08=%23country%2Bname%2C%23adm1%2Bname&tagger-match-all=on&tagger-default-tag=%23affected%2Blabel&tagger-01-header=province%2Fstate&tagger-01-tag=%23adm1%2Bname&tagger-02-header=country%2Fregion&tagger-02-tag=%23country%2Bname&tagger-03-header=lat&tagger-03-tag=%23geo%2Blat&tagger-04-header=long&tagger-04-tag=%23geo%2Blon&header-row=1&url=https%3A%2F%2Fraw.githubusercontent.com%2FCSSEGISandData%2FCOVID-19%2Fmaster%2Fcsse_covid_19_data%2Fcsse_covid_19_time_series%2Ftime_series_covid19_deaths_global.csv"
+
+processDataset <- function(df,min_treshold){
+  names(df) <- tolower(names(df))
+  df <- df[-1,]
+  df$date <- as.Date(df$date, "%Y-%m-%d")
+  df$value <- as.numeric(df$value)
+  df <- select(df, date, iso.3166.1.alpha.3.codes, province.state, value)
+  names(df) <- c("date","country","province","value")
+  df$one <- 1
+  
+  TRESHOLD <- 1
+  
+  df.sum <- df %>% 
+    arrange(date) %>%
+    filter(value >= min_treshold) %>%
+    group_by(date,country) %>%
+    summarise(
+      value=sum(value),
+      one=1
+    ) %>%
+    group_by(country) %>%
+    mutate(daysSinceStart = cumsum(one)) 
+  
+  return(df.sum)  
+}
+
+TRESHOLD_CASES <- 100
+cases <- read.csv(casesUrl,as.is=T)
+cases.by.day <- processDataset(cases,TRESHOLD_CASES)
+
+TRESHOLD_DEATHS <- 1
+deaths <- read.csv(deathsUrl,as.is=T)
+deaths.by.day <- processDataset(deaths,TRESHOLD_DEATHS)
+
+countries.by.cases <- cases.by.day %>%
+  group_by(country) %>%
+  summarise(
+    maxCases = max(value),
+    firstCase = min(date),
+  ) %>% arrange( desc(maxCases))
+
+
+cases.by.day %>% filter(country %in% c("CZE","DEU","ITA","SPA","GBR", "AUS", "POL","SWE")) %>%
+ggplot(aes(x=daysSinceStart,y=value,))+geom_line(aes(col=country))+
+  #scale_y_continuous(labels=comma, trans = "log10")+
+  scale_y_continuous(labels=comma, limits=c(0,20000))+
+  theme_bw()+
+  labs(y="Confirmed cases",x=sprintf("Days since %d cases",TRESHOLD_CASES))
+
+deaths.by.day %>% filter(country %in% c("CZE","DEU","ITA","SPA","GBR", "AUS", "POL","SWE")) %>%
+  ggplot(aes(x=daysSinceStart,y=value,))+geom_line(aes(col=country))+
+  #scale_y_continuous(labels=comma, trans = "log10")+
+  scale_y_continuous(labels=comma, limits=c(0,10000))+
+  theme_bw()+
+  labs(y="Confirmed cases",x=sprintf("Days since %d cases",TRESHOLD_DEATHS))
+
+names(cases)
